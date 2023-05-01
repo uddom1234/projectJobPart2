4a392cd960c3631cb097aa821f73ef0b2b48e2f6
Uddom <uddompang@gmail.com>
2023-05-01 14:33:44 +1000

    fixed up apply page

--- processEOI.php
+++ processEOI.php
@@ -25,29 +25,33 @@ CREATE TABLE IF NOT EXISTS eoi (
 )";
 
 if (mysqli_query($conn, $sql_create_table) === FALSE) {
-    echo "Error creating table: " . mysqli_error($conn);
+    echo "Error creating table";
 }
 
-    $job_reference_number = $_POST['job_reference_number'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $date_of_birth = $_POST['date_of_birth'];
+    $jobReferenceNumber = $_POST['jobReferenceNumber'];
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $dateOfBirth = $_POST['dateOfBirth'];
     $gender = $_POST['gender'];
-    $street_address = $_POST['street_address'];
-    $suburb_town = $_POST['suburb_town'];
+    $streetAddress = $_POST['streetAddress'];
+    $suburbTown = $_POST['suburbTown'];
     $state = $_POST['state'];
     $postcode = $_POST['postcode'];
-    $email_address = $_POST['email_address'];
-    $phone_number = $_POST['phone_number'];
-    $skills = $_POST['skills'];
-    $other_skills = $_POST['other_skills'];
+    $emailAddress = $_POST['emailAddress'];
+    $phoneNumber = $_POST['phoneNumber'];
+    $skills = implode(", ", $_POST['skills']);
+    $otherSkills = $_POST['otherSkills'];
 
 
 //need to add validation
+$error = [];
 
-// Insert the record into the eoi table
-$sql_insert = "INSERT INTO eoi (JobReferenceNumber, FirstName, LastName, DateOfBirth, Gender, StreetAddress, SuburbTown, State, Postcode, EmailAddress, Skills, PhoneNumber, OtherSkills)
-VALUES ('$job_reference_number', '$first_name', '$last_name', '$date_of_birth', '$gender', '$street_address', '$suburb_town', '$state', '$postcode', '$email_address', '$phone_number', '$other_skills')";
+if (empty($erorr)) {
+    // Insert the record into the eoi table
+    $sql_insert = "INSERT INTO eoi (JobReferenceNumber, FirstName, LastName, DateOfBirth, Gender, StreetAddress, SuburbTown, State, Postcode, EmailAddress, Skills, PhoneNumber, OtherSkills)
+    VALUES ('$job_reference_number', '$first_name', '$last_name', '$date_of_birth', '$gender', '$street_address', '$suburb_town', '$state', '$postcode', '$email_address', '$phone_number', '$other_skills')";
+} 
+
 
 if (mysqli_query($conn, $sql_insert)) {
     echo "EOI record created successfully.";
