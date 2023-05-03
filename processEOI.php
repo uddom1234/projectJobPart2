aeadceca82fef5dcdf7b98db7e5cb1e5b08cb99b
rehnumish <104313715@student.swin.edu.au>
2023-05-03 19:18:00 +1000

    added validation

--- processEOI.php
+++ processEOI.php
@@ -28,28 +28,15 @@ if (mysqli_query($conn, $sql_create_table) === FALSE) {
     echo "Error creating table";
 }
 
-    $jobReferenceNumber = $_POST['jobReferenceNumber'];
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $dateOfBirth = $_POST['dateOfBirth'];
-    $gender = $_POST['gender'];
-    $streetAddress = $_POST['streetAddress'];
-    $suburbTown = $_POST['suburbTown'];
-    $state = $_POST['state'];
-    $postcode = $_POST['postcode'];
-    $emailAddress = $_POST['emailAddress'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $skills = implode(", ", $_POST['skills']);
-    $otherSkills = $_POST['otherSkills'];
-
 
 //need to add validation
-$error = 0;
+$error = array;
 if (isset($_POST["jobReferenceNumber"])){
     $jobReferenceNumber=$_POST["jobReferenceNumber"];
 }
 else{
     echo "<p> Enter data in the <a href=\"apply.php\">form</a></p>";
+    array_push($error,)
 }
 if (isset($_POST["firstname"])){
     $firstname=$_POST["firstname"];
@@ -127,8 +114,12 @@ if (isset($_POST["English"])) $skills=$skills."Fluency in English";
 
 // Insert the record into the eoi table
 
-$sql_insert = "INSERT INTO eoi (JobReferenceNumber, FirstName, LastName, DateOfBirth, Gender, StreetAddress, SuburbTown, State, Postcode, EmailAddress, PhoneNumber, Skills, OtherSkills, Status)
+if($error == 0) {
+    $sql_insert = "INSERT INTO eoi (JobReferenceNumber, FirstName, LastName, DateOfBirth, Gender, StreetAddress, SuburbTown, State, Postcode, EmailAddress, PhoneNumber, Skills, OtherSkills, Status)
     VALUES ('$jobReferenceNumber', '$firstName', '$lastName', '$dateOfBirth', '$gender', '$streetAddress', '$suburbTown', '$state', '$postcode', '$emailAddress', '$phoneNumber', '$skills', '$otherSkills', 'New')";
+} else {
+    echo "There's an error dumbass!"
+}
 
 if (mysqli_query($conn, $sql_insert)) {
     echo "EOI record created successfully.";
