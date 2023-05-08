d959bd767126bace00c3b65b07e3cef53dd36481
Ben Taylor <bbtnz1@gmail.com>
2023-05-08 15:26:51 +1000

    changes

--- jobs.php
+++ jobs.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ye</title>
-	<link href="style.css" rel="stylesheet"  media="screen"/>
     <title>Jobs</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/x-icon" href="/images/logo.png">
+    <link href="style.css" rel="stylesheet"  media="screen"/>
 </head>
 <body>
 	<?php 
