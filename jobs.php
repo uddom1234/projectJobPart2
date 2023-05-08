06c29776fd4dcd4d3da11a43390190d0af183788
Ben Taylor <bbtnz1@gmail.com>
2023-05-08 14:55:39 +1000

    changes to jobs.php

--- jobs.php
+++ jobs.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ye</title>
 	<link href="style.css" rel="stylesheet"  media="screen"/>
-    <title>jobs</title>
+    <title>Jobs</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="/images/logo.png">
 </head>
